ec98f9f157952d04ff0341d68ca17d6656bf80d6
Nguyen Thu Hang <thuhangcs013@gmail.com>
2026-07-15 03:39:33 +0800

    fix import

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,6 +1,7 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { IoMdClose } from "react-icons/io";
 
+
 interface ModalProps {
     isOpen: boolean;
     onChange: (open: boolean) => void;
